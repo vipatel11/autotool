99eb89300b8015b1c98637f7b27c13956612f0a0
rpatel1291 <rpatel1291@gmail.com>
2022-06-11 22:46:11 -0400

    Fix V's code

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,80 +1,114 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import { signIn } from '../../store/actions/authActions'
-import { Redirect } from "react-router-dom"
-import jwt_decode from "jwt-decode"
+import { signIn, signUpGmail } from '../../store/actions/authActions'
+import { Redirect } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
 
+const SignIn = (props) => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [givenName, setGivenName] = useState('')
+	const [familyName, setFamilyName] = useState('')
+	const [pictureUrl, setPictureUrl] = useState('')
 
-function SignIn() {
+	const handleEmailChange = (e) => {
+		setEmail(e.target.value)
+	}
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value); 
-    };
+	const handleLoginChangeFromResponse = (userObject) => {
+		setEmail(userObject.email)
+		setGivenName(userObject.givenName)
+		setFamilyName(userObject.familyName)
+		setPictureUrl(userObject.picture)
+	}
 
-    const handlePasswordChange = (e) => {
-      setPassword(e.target.value); 
-  };
+	const handlePasswordChange = (e) => {
+		setPassword(e.target.value)
+	}
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log("jk");
-      console.log([email,password]);
-  }
-    
-    function handleCallbackResponse(response){
-        console.log("Encoded JWT ID token: " + response.credential);
-        var userObject = jwt_decode(response.credential);
-        console.log(userObject);
-    } 
-      
-    useEffect(()=> {
-            /* global google */
-            google.accounts.id.initialize({
-              client_id: "351269892640-m1lf2srm7sfqo4b6fcp02c4vovtptvod.apps.googleusercontent.com",
-              callback: handleCallbackResponse
-            });
-      
-            google.accounts.id.renderButton(
-              document.getElementById("signInDiv"),
-              {
-                theme: "outline", size: " extralarge"
-              }
-            )
-      
-    },[]);
-    return (
-            <div className="container">    
-                <form onSubmit={handleSubmit} className="col s12 m6">
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-                    <div className="input-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={handleEmailChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={handlePasswordChange} />
-                    </div>
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login</button>
-                    </div>
-                    <div id = "signInDiv"></div>
-                </form>
-            </div>
-        );
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		let credential = {
+			familyName: familyName,
+			givenName: givenName,
+			email: email,
+			password: password,
+			pictureUrl: pictureUrl,
+		}
+		props.signIn(credential)
+	}
 
-    };
-    const mapStateToProps = (state) => {
-      return {
-          authError: state.auth.authError,
-          auth: state.firebase.auth,
-      }
-  }
-  const mapDispatchToProps = (dispatch) => {
-    return {
-          signIn: ([email,password]) => dispatch(signIn([email,password]))
-      }
-  }
-  export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
+	const handleGmailSignIn = (e) => {
+		e.preventDefault()
+		let credential = {
+			familyName: familyName,
+			givenName: givenName,
+			email: email,
+			pictureUrl: pictureUrl,
+		}
+		props.signUpGmail(credential)
+	}
+
+	function handleCallbackResponse(response) {
+		//console.log('Encoded JWT ID token: ' + response.credential)
+		handleLoginChangeFromResponse(jwt_decode(response.credential))
+	}
+
+	useEffect(() => {
+		/* global google */
+		google.accounts.id.initialize({
+			client_id:
+				'351269892640-m1lf2srm7sfqo4b6fcp02c4vovtptvod.apps.googleusercontent.com',
+			callback: handleCallbackResponse,
+		})
+
+		google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+			theme: 'outline',
+			size: ' extralarge',
+		})
+	}, [])
+	const { authError, auth } = props
+	if (auth.uid) return <Redirect to='/' />
+	return (
+		<div className='container'>
+			<form onSubmit={handleSubmit} className='col s12 m6'>
+				<h5 className='grey-text text-darken-3'>Sign In</h5>
+				<div className='input-field'>
+					<label htmlFor='email'>Email</label>
+					<input
+						type='email'
+						id='email'
+						onChange={handleEmailChange}
+					/>
+				</div>
+				<div className='input-field'>
+					<label htmlFor='password'>Password</label>
+					<input
+						type='password'
+						id='password'
+						onChange={handlePasswordChange}
+					/>
+				</div>
+				<div className='input-field'>
+					<button className='btn pink lighten-1 z-depth-0'>
+						Login
+					</button>
+				</div>
+				<div id='signInDiv' onClick={handleGmailSignIn}></div>
+			</form>
+		</div>
+	)
+}
+const mapStateToProps = (state) => {
+	return {
+		authError: state.auth.authError,
+		auth: state.firebase.auth,
+	}
+}
+const mapDispatchToProps = (dispatch) => {
+	return {
+		signIn: (credential) => dispatch(signIn(credential)),
+		signUpGmail: (credential) => dispatch(signUpGmail(credential)),
+	}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,50 +1,60 @@
-export const signIn = (email,password) => {
+export const signIn = (credential) => {
+	return (dispatch, getState, { getFirebase }) => {
+		const firebase = getFirebase()
 
-    return (dispatch, getState, { getFirebase }) => {
-        const firebase = getFirebase();
-
-        firebase.auth().signInWithEmailAndPassword(
-            email.email,
-            password.password
-        ).then(() => {
-            console.log("YOU SIGNED IN")
-            dispatch({ type: 'LOGIN_SUCCESS' });
-        }).catch((err) => {
-            dispatch({ type: 'LOGIN_ERROR', err });
-        });
-    }
-
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(credential.email, credential.password)
+			.then(() => {
+				console.log('YOU SIGNED IN')
+				dispatch({ type: 'LOGIN_SUCCESS' })
+			})
+			.catch((err) => {
+				dispatch({ type: 'LOGIN_ERROR', err })
+			})
+	}
 }
 
 export const signOut = () => {
-    return (dispatch, getState, { getFirebase }) => {
-        const firebase = getFirebase();
+	return (dispatch, getState, { getFirebase }) => {
+		const firebase = getFirebase()
 
-        firebase.auth().signOut().then(() => {
-            dispatch({ type: 'SIGNOUT_SUCCESS' });
-        });
-    }
+		firebase
+			.auth()
+			.signOut()
+			.then(() => {
+				dispatch({ type: 'SIGNOUT_SUCCESS' })
+			})
+	}
 }
 
 export const signUp = (newUser) => {
-    return (dispatch, getState, { getFirebase, getFirestore }) => {
-        const firebase = getFirebase();
-        const firestore = getFirestore();
+	return (dispatch, getState, { getFirebase, getFirestore }) => {
+		const firebase = getFirebase()
+		const firestore = getFirestore()
 
-        firebase.auth().createUserWithEmailAndPassword(
-            newUser.email,
-            newUser.password
-        ).then((resp) => {
-            return firestore.collection('users').doc(resp.user.uid).set({
-                firstName: newUser.firstName,
-                lastName: newUser.lastName,
-                initials: newUser.firstName[0] + newUser.lastName[0]
-            })
-        }).then(() => {
-            dispatch(
-                { type: 'SIGNUP_SUCCESS' })
-        }).catch(err => {
-            dispatch({ type: 'SIGNUP_ERROR', err })
-        })
-    }
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(newUser.email, newUser.password)
+			.then((resp) => {
+				return firestore
+					.collection('users')
+					.doc(resp.user.uid)
+					.set({
+						firstName: newUser.firstName,
+						lastName: newUser.lastName,
+						initials: newUser.firstName[0] + newUser.lastName[0],
+					})
+			})
+			.then(() => {
+				dispatch({ type: 'SIGNUP_SUCCESS' })
+			})
+			.catch((err) => {
+				dispatch({ type: 'SIGNUP_ERROR', err })
+			})
+	}
+}
+
+export const signUpGmail = (newUser) => {
+	//TODO
 }
